3af0101f0e8283a7a6bcc9133414d910c583094e
Sadi_er59 <Sadi_er59@itstep.edu.az>
2021-08-12 21:54:50 -0700

    Add project files.

---
+++ BridgeTask/Classes/IRemote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeTask.Classes
+{
+    interface IRemote
+    {
+        void Power(bool IsEnable);
+
+        void VolumeDown();
+
+        void VolumeUP();
+
+        void ChannelDown();
+
+        void ChannelUp();
+
+        void Mute(bool IsMute);
+    }
+}
